5fad16b693409a01ad9815c08b8e7dfb4aee8aaf
Tommy Rosario <tomjasonrosario@gmail.com>
2021-02-03 23:22:02 -0500

    updated favicon

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,16 @@
 import { Container } from "@chakra-ui/react"
 import Head from 'next/head';
+import { BASE_PATH } from "../constants/app";
 
 export default function Layout({ children, home }) {
   return (
-    <Container maxWidth="5xl">
-      {children}
-    </Container>
+    <>
+      <Head>
+        <link rel="shortcut icon" href={`${BASE_PATH}/memoji.png`} />
+      </Head>
+      <Container maxWidth="5xl">
+        {children}
+      </Container>
+    </>
   );
 }
