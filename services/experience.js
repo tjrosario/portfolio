4c0f95922956f98e3dc614347e6928ca4e5c920d
Tommy Rosario <tomjasonrosario@gmail.com>
2021-02-06 14:03:23 -0500

    enable api routes

--- services/experience.js
+++ services/experience.js
@@ -1,9 +1,7 @@
 import { ABSOLUTE_PATH } from '../constants';
 
 export async function getExperience() {
-/*   const data = await fetch(`${ABSOLUTE_PATH}/api/experience`);
+  const data = await fetch(`${ABSOLUTE_PATH}/api/experience`);
   const json = await data.json();
-  return json; */
-
-  return [];
+  return json;
 }
