7aa67444c6d3630d1cfc951c4c05532e3b666545
Tommy Rosario <tom.jason.rosario@gmail.com>
2024-03-06 21:41:19 -0500

    add new work sample, convert to yarn

--- pages/api/work.js
+++ pages/api/work.js
@@ -1,10 +1,11 @@
 const WORK = [
     {
-        description: 'Fun little brewery app that lets you search breweries across the country using different sets of criteria. Built using React\'s context and hooks API\'s.',
-        github: 'https://github.com/tjrosario/brewery-app',
-        id: 0,
-        preview: 'img/work/brewery.jpg',
-        title: 'Brewery App'
+        description: 'Algolia-powered search engine built with React and NextJS that searches both blog articles and doula service providers.',
+        id: 1,
+        preview: 'img/work/cake.jpg',
+        src: 'video/cake-search.mp4',
+        title: 'Multi-facet / Multi-index search',
+        url: 'https://www.joincake.com/blog'
     },
     {
         description: 'Threadlab is an eco-friendly lifestyle personalization technology that makes it easy to discover the best new products from around the world.',
@@ -16,8 +17,15 @@ const WORK = [
         url: 'https://www.mythreadlab.com'
     },
     {
-        description: 'Online proofing system tailored for helping SMBs deliver marketing projects efficiently by streamlining reviewing and approving creative content.',
+        description: 'Fun little brewery app that lets you search breweries across the country using different sets of criteria. Built using React\'s context and hooks API\'s.',
+        github: 'https://github.com/tjrosario/brewery-app',
         id: 3,
+        preview: 'img/work/brewery.jpg',
+        title: 'Brewery App'
+    },
+    {
+        description: 'Online proofing system tailored for helping SMBs deliver marketing projects efficiently by streamlining reviewing and approving creative content.',
+        id: 4,
         preview: 'img/work/smb.png',
         src: 'video/proofs.mp4',
         title: 'Proofing System for SMBs'
@@ -25,7 +33,7 @@ const WORK = [
     {
         description: 'A fun little app that allows you to search the Giphy Translate API by supplying a "weirdness" level. You can add any GIFs to your favorites list and generate an average "weirdness score."',
         github: 'https://github.com/tjrosario/giphy',
-        id: 4,
+        id: 5,
         preview: 'img/work/giphy.svg',
         src: 'video/giphy.mp4',
         title: 'Giphy Weirdness Calculator'
